6371d81e94e497e7885c6a000d289f1f003dcb90
Pawelec Rafał 2 - Detal <Rafal.Pawelec2@telekomunikacja.pl>
2018-05-24 11:51:03 +0200

    Nowe zmiany

---
+++ projekt-js/js/main.js
@@ -0,0 +1,8 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+var name = "Jan Nowak";
+console.log(name);
+
+document.querySelector('h1').onclick = function() {
+alert('Właśnie mnie kliknąłeś');
+}
